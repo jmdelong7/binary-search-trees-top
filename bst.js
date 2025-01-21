cee4846f68b312c72f377533d7a90a2117fe6bb4
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-01-20 16:05:41 -0800

    Node, Tree, buildTree - Balanced BST

--- bst.js
+++ bst.js
@@ -10,3 +10,37 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
   }
 };
+
+class Node {
+  constructor(data = null) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = buildTree(array);
+  }
+}
+
+function buildTree(array, start = 0, end = array.length - 1) {
+  if (start > end) return null;
+  const mid = Math.floor((start + end) / 2); // index = 5, value = 8
+  const root = new Node(array[mid]);
+
+  root.left = buildTree(array, start, mid - 1);
+  root.right = buildTree(array, mid + 1, end);
+
+  return root;
+  // root.left = buildTree(array.slice(start, mid));
+  // root.right = buildTree(array.slice(mid + 1, end));
+  // return root;
+}
+
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]; // 14
+array = [...new Set(array)].sort((a, b) => a - b); // 11
+// [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345]
+const tree = new Tree(array);
+prettyPrint(tree.root);
