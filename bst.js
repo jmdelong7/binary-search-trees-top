00a7c682a8019f552f8db01c07e36bc5006c5f72
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-01-22 21:14:18 -0800

    Tree.delete
    Deletes leaf nodes

--- bst.js
+++ bst.js
@@ -38,14 +38,16 @@ class Tree {
   }
 
   delete(root, value) {
-    // need to set root.right or .left to null
-    // depending on which side the value I'm searching for is on.
-    // right now, root is the value I want to delete, I need the parent.
-    if (value === root.data && !root.left && !root.right) {
-      console.log(root);
-      root = null;
-      return;
+    if (root === null) return;
+
+    if (root.left !== null) {
+      value === root.left.data ? (root.left = null) : null;
     }
+
+    if (root.right !== null) {
+      value === root.right.data ? (root.right = null) : null;
+    }
+
     if (value < root.data) return this.delete(root.left, value);
     if (value > root.data) return this.delete(root.right, value);
   }
@@ -72,5 +74,5 @@ tree.insert(tree.root, 16);
 tree.insert(tree.root, 17);
 tree.insert(tree.root, 10);
 tree.insert(tree.root, -1);
-tree.delete(tree.root, -1);
+tree.delete(tree.root, 44);
 prettyPrint(tree.root);
