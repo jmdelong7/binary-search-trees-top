e9c77cfb0d0bbece0c1162a94b8ce564ddafb5b6
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2025-01-25 11:12:35 -0800

    deleteVal()
    Figured out leaf case and 1 child case but couldn't figure out 2 child case.
    Looked up solution for 2 child case.
    Will replace function with more concise version.

--- bst.js
+++ bst.js
@@ -50,6 +50,64 @@ function insert(root, value) {
   if (value > root.data) return insert(root.right, value);
 }
 
+function deleteVal(root, value) {
+  if (!root.left && !root.right) return null;
+
+  const { left, right } = root;
+
+  if (left && left.data === value) {
+    const l = left.left;
+    const r = left.right;
+
+    // Delete left node that has 2 children
+    if (l && r) {
+      let successor = r;
+      while (successor.left) {
+        successor = successor.left;
+      }
+      left.data = successor.data;
+      left.right = deleteVal(r, successor.data);
+      return root;
+    }
+
+    // Delete left node that has 1 child
+    if (l && !r) return (root.left = l);
+    if (!l && r) return (root.left = r);
+
+    // Delete left node that is a leaf node
+    return (root.left = null);
+  }
+
+  if (right && right.data === value) {
+    const l = right.left;
+    const r = right.right;
+
+    // Delete right node that has 2 children
+    if (l && r) {
+      let successor = r;
+      while (successor.left) {
+        successor = successor.left;
+      }
+      right.data = successor.data;
+      right.right = deleteVal(r, successor.data);
+      return root;
+    }
+
+    // Delete right node that has 1 child
+    if (l && !r) return (root.right = l);
+    if (!l && r) return (root.right = r);
+
+    // Delete right node that is a leaf node
+    return (root.right = null);
+  }
+
+  if (value < root.data) {
+    return deleteVal(root.left, value);
+  } else if (value > root.data) {
+    return deleteVal(root.right, value);
+  }
+}
+
 function find(root, value) {
   if (value === root.data) return root;
   if (root === null) return null;
@@ -84,11 +142,20 @@ function height(node) {
   return Math.max(height(node.left), height(node.right)) + 1;
 }
 
+function depth(root, node) {
+  if (!root || !node) return -1;
+  if (node.data < root.data) return depth(root.left, node);
+  if (node.data > root.data) return depth(root.right, node);
+}
+
 let array = [
   1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 25, 84, 234, 113, 6345, 324, 313, 555,
   444,
 ]; // 14
 const tree = new Tree(array);
+deleteVal(tree.root, 6345);
+deleteVal(tree.root, 324);
+deleteVal(tree.root, 113);
+deleteVal(tree.root, 5);
+
 prettyPrint(tree.root);
-console.log(tree.root.left.left.left);
-console.log(height(tree.root.left.left.left));
